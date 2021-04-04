5d8310c2ec790a90af3727d1e4f87822f650521b
Grady Robbins <gradyrobbins13@gmail.com>
2021-04-03 23:07:48 -0500

    stuff

--- src/scripts/JournalForm.js
+++ src/scripts/JournalForm.js
@@ -2,25 +2,28 @@ const FormManager = Object.create(null, {
     clearForm: {
         value: () => {
             document.querySelector("#entryTitle").value = " ",
-            document.querySelector("#entryContent").value = " "
+                document.querySelector("#entryContent").value = " "
         }
     },
 
     renderEntryForm: {
         value: () => {
 
-        return `
-        <fieldset>
+            return `<form class="pure-form" integrity="">
+        <fieldset class="pure-group">
             <label for="entryTitle">Title:</label>
-            <input required type="text" id="entryTitle">
+            <input required type="text" id="entryTitle" class="pure-input-1-2" placeholder="Title"  >
         </fieldset>
 
         <fieldset>
             <label for="entryContent">Deep thoughts:</label>
-            <textarea id="entryContent"></textarea>
+            <textarea id="entryContent" class="pure-input-1-2" placeholder="Deep thoughts" ></textarea>
         </fieldset>
 
-        <button id="saveEntryButton">Save Journal Entry</button>`
+        <button id="saveEntryButton" type="submit" class="pure-button pure-input-1-2 pure-button-primary>Save Journal Entry</button>
+
+    </fieldset>
+</form>`
         }
     }
 })
